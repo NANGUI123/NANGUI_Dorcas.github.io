f91f40dd13b4eb34efbf373fa4276b28c105b42f
NANGUI123 <42325652+NANGUI123@users.noreply.github.com>
2024-04-05 00:02:47 +0200

    Add new file index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Accueil</title>
+    <?php include("View/include/header.php"); ?>    
+</head>
+<body>
+<?php include("View/include/navbar.php"); ?>    
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h1>Bienvenue sur mon portfolio</h1>
+    </div>
+    <div class="card">
+      <h2>Dorcas NANGUI SACADOURA</h2>
+      <h4>Étudiante en deuxième année de BTS SIO option SLAM</h4></br>
+      <p>Ce portfolio met à votre disposition mes compétences, mon parcours professionnel, mes projets et mes réalisations</p>
+
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h3>CFA Cerfal Campus Montsouris</h3></br>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.7454242559124!2d2.3285334748983733!3d48.824918571327615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671a566eea501%3A0xe199b33dda2e24eb!2sCFA%20Cerfal%20-%20Campus%20Montsouris!5e0!3m2!1sfr!2sfr!4v1684998543306!5m2!1sfr!2sfr" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    </div>
+    <div class="card">
+      <h3>Mes établissements</h3>
+        <p><a href="https://www.diderot-education.fr"><img width="300" height="300" src="images/10527_logo_rs.gif"></a><a href="https://www.campusmontsouris.fr/"><img width="249" height="170" src="images/logo_campus.png"></a></p>
+        
+    </div>
+  </div>
+</div>
+
+<?php include("View/include/footer.php"); ?>    
+</body>
+</html>
