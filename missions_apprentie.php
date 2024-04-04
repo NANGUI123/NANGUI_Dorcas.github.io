435a282faf9ce56a86b65b4344ab39f4fd5c838b
NANGUI123 <42325652+NANGUI123@users.noreply.github.com>
2024-04-05 00:21:02 +0200

    Add files via upload

---
+++ missions_apprentie.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Missions réalisées en formation</title>
+    <?php include("View/include/header.php"); ?>    
+</head>
+<body>
+<?php include("View/include/navbar.php"); ?>
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>Missions réalisées en formation</h2></br>
+      <h4><ol>• Déploiement d'une page d'accueil web “Agence de voyage . " (Html 5 et Css)</ol></h4>
+      <i class="fa fa-file-pdf-o" aria-hidden="true"> <a href="images/Fiche_1.pdf" download="Fiche_mission_1.pdf" style="text-decoration: none;">Mission 1</a></i></br>
+      <h4><ol>• PPE 1 : Gestion libre du Parc informatique GLPI et développement d'une page de connexion web (Html et Css) de l'entreprise TeamStadium</ol></h4>
+      <i class="fa fa-file-pdf-o" aria-hidden="true"> <a href="images/GLPI.pptx.pdf" download="Fiche_mission_2.pdf" style="text-decoration: none;">Mission 2</a></i></br>
+      <h4><ol>• Développement du jeu de Mastermind en C# sur visual studio</ol></h4>
+      <i class="fa fa-file-pdf-o" aria-hidden="true"> <a href="images/Fiche_3.pdf" download="Fiche_mission_3.pdf" style="text-decoration: none;">Mission 3</a></i></br>
+      <h4><ol>• Développement du jeu de Morpion en C# sur visual studio</ol></h4>
+      <i class="fa fa-file-pdf-o" aria-hidden="true"> <a href="images/TP1 Morpion (1).pdf" download="Fiche_mission_4.pdf" style="text-decoration: none;">Mission 4</a></i></br>
+      <h4><ol>• PPE 2 : installation de windows server 2016 et Centos sous une machine virtuelle (VirtualBox)</ol></h4>
+      <i class="fa fa-file-pdf-o" aria-hidden="true"> <a href="images/Fiche_5.pdf" download="Fiche_mission_5.pdf" style="text-decoration: none;">Mission 5</a></i></br>
+      <h4><ol>• Déploiement d'une calculatrice sous WPF en C#</ol></h4>
+      <i class="fa fa-file-pdf-o" aria-hidden="true"> <a href="images/Fiche_6.pdf" download="Fiche_mission_6.pdf" style="text-decoration: none;">Mission 6</a></i></br>
+      <h4><ol>• Déploiement d'une application Web "Belle Table" (Html, CSS et PHP)</ol></h4>
+      <i class="fa fa-file-pdf-o" aria-hidden="true"> <a href="Fiche_mission_7.pdf" download="Fiche_mission_1.pdf" style="text-decoration: none;">Mission 7</a></i></br>
+      <h4><ol>• PPE 4 : Déploiement d'une application en C# sous WPF "Application de Gestion du fonds documentaire : Belle Table" en mode MVVC</ol></h4>
+      <i class="fa fa-file-pdf-o" aria-hidden="true"> <a href="images/Fiche_8.pdf" download="Fiche_mission_8.pdf" style="text-decoration: none;">Mission 8</a></i></br>
+
+    </div>
+  </div>
+</div>    
+<?php include("View/include/footer.php"); ?>    
+</body>
+</html>
